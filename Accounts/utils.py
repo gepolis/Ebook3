1e27b41e72d03ec33cbff6dcc551fa07b491311b
Ivan A <120267018+gepolis@users.noreply.github.com>
2023-08-31 11:02:03 +0300

    update

--- Accounts/utils.py
+++ Accounts/utils.py
@@ -25,6 +25,7 @@ if settings.SERV:
     options.add_argument("--start-maximized")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
+    options.add_argument("-disable-dev-shm-usage")
 options.add_argument("--disable-blink-features=AutomationControlled")
 selpath = "/home/vanua/PycharmProjects/django/VolunteerE-book/chromedriver"
 if settings.SERV:
