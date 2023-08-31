7b20ed3ce50d1802c5ed6b2d70aad57b48a6a01a
Ivan A <120267018+gepolis@users.noreply.github.com>
2023-08-31 15:12:18 +0300

    upd

--- MainApp/views.py
+++ MainApp/views.py
@@ -10,3 +10,17 @@ from Accounts.models import Account
 def index(request):
     return render(request, "main.html")
 
+def feedback(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+        headers = {
+                    #"Authorization": "MTA2Njg0NDI5OTUxMTIyMjI5NA.G8T_2D.HCBJ-AP1RhRXABiJhWfTS80SD1kJ8Gk4QHA6eo",
+                    "Authorization": "Bot MTE0NTQ3MTMwMDcwMjMyNjkzNw.GQSnnl.SJt9a0Ul8kxCZVvnBgnXcdV3EcsS4tfnM_WnQU",
+                    "content-encoding": "utf-8",
+        }
+        r = requests.post("https://discord.com/api/v8/channels/1145459123270459513/messages",
+                            headers=headers, data={"content": f"Запрос, от {name}\n\nТел: {phone}\nПочта: {email}\n\n{message}"})
+        return redirect("/")
